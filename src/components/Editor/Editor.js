61ccb7612b0b119540b474d30dc8bcfcd9f78d40
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-14 14:24:20 -0500

    Handle subtitle variable

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor({ title, setTitle }) {
+export default function Editor({ title, setTitle, subtitle, setSubtitle }) {
   return (
     <div className="editor">
       <div className="form-control">
@@ -10,7 +10,12 @@ export default function Editor({ title, setTitle }) {
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
-        <input type="text" />
+        <input
+          name="subtitle"
+          type="text"
+          value={subtitle}
+          onChange={(e) => setSubtitle(e.target.value)}
+        />
         <label>Subtitle</label>
       </div>
       <div className="form-control">
